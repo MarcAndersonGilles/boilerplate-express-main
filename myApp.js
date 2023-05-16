d919f7b662eb82ad380c394b67d9e69c52464ada
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-05-15 20:16:33 -0400

    practicing get method, env process file, etc

--- myApp.js
+++ myApp.js
@@ -1,5 +1,23 @@
 let express = require('express');
+require('dotenv').config();
 let app = express();
+
+app.get('/', function(req, res) {
+    // res.send('Tu es sur la page principal de mon application Express');
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        res.json({"message": "HELLO JSON"});
+    }else{
+        res.json({"message": "Hello json"});
+    }
+    
+
+})
+app.use('/public',express.static(__dirname + '/public'));
+
+
 
 console.log("Hello World");
 
