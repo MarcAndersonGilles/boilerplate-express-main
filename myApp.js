ae19a7908071b7f33aa7064e5aa30187bd3abf79
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-05-18 17:20:40 -0400

    envoyer donnee formulaire avec POST

--- myApp.js
+++ myApp.js
@@ -1,6 +1,27 @@
 let express = require('express');
+let bodyParser = require('body-parser')
 require('dotenv').config();
 let app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use((req,res,next) => {
+    console.log("Method: ", req.method, "Path: ", req.path, "IP: ", req.ip);
+    next();
+})
+app.get('/now', (req,res, next) => {
+    req.time = new Date().toISOString();
+    next();
+
+},(req,res) => {
+    res.send({time: req.time});
+    
+})
+app.get('/echo/:id', (req,res) => {
+    res.send('Tu écoutes la rêquete de id:' + " " + req.params.id);
+})
+app.get('/name', (req,res) => {
+    res.send({name: req.query.first + " " + req.query.last});
+})
 
 app.get('/', function(req, res) {
     // res.send('Tu es sur la page principal de mon application Express');
@@ -17,6 +38,10 @@ app.get('/json', (req, res) => {
 })
 app.use('/public',express.static(__dirname + '/public'));
 
+/// enovoie les données d'un formulaire sur la route /name
+app.post('/name', (req,res) => {
+    res.send({name: req.body.first + '' + req.body.last})
+})
 
 
 console.log("Hello World");
